98fa653de6c6dfd4e593059c61aab73a8dc74143
jespytall <jespytall@protonmail.com>
2023-04-19 19:54:46 -0500

    started javadoc for Enemy class

--- app/src/main/java/edu/utsa/cs3443/dungeon/Enemy.java
+++ app/src/main/java/edu/utsa/cs3443/dungeon/Enemy.java
@@ -5,6 +5,8 @@ package edu.utsa.cs3443.dungeon;
 public class Enemy extends Entity {
 
     /**
+     * Defines the Entity class and relevant methods
+     * @param whatever idk
      */
     public Enemy(final String _name, final int _maxHP, final char _smallCharacter)
     {
